7a4803486ccc430e8baeaa9411e36c35cab0aecb
rabobahago <rabobahago@gmail.com>
2020-09-27 17:27:07 +0100

    use effect hook added

--- src/useFetchJobs.js
+++ src/useFetchJobs.js
@@ -1,4 +1,5 @@
-import { useReducer } from 'react'
+import { useReducer, useEffect } from 'react'
+import axios from 'axios'
 const ACTIONS = {
   MAKE_REQUEST: 'make-request',
   GET_DATA: 'get-data',
@@ -6,10 +7,33 @@ const ACTIONS = {
 }
 const reducer = (state, action) => {
   switch (action.type) {
+    case ACTIONS.MAKE_REQUEST:
+      return {
+        loading: true,
+        jobs: [],
+      }
+    case ACTIONS.GET_DATA:
+      return {
+        ...state,
+        loading: false,
+        jobs: action.payload.jobs,
+      }
+    case ACTIONS.ERROR:
+      return {
+        ...state,
+        loading: false,
+        error: action.payload.error,
+        jobs: [],
+      }
+    default:
+      state
   }
 }
 const useFetchJobs = (params, page) => {
   const [state, dispatch] = useReducer(reducer, { jobs: [], loading: true })
+  useEffect(() => {
+    dispatch({ type: ACTIONS.MAKE_REQUEST })
+  }, [params, page])
   return {
     jobs: [],
     loading: false,

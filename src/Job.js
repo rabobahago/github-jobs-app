edd108409bdbfd1de6693f069bf8f517f926359c
rabobahago <rabobahago@gmail.com>
2020-10-06 17:21:12 +0100

    Card Button and others react bootstrap added

--- src/Job.js
+++ src/Job.js
@@ -1,6 +1,8 @@
-import React from 'react'
-import { Card } from 'react-bootstrap'
+import React, { useState } from 'react'
+import { Card, Badge, Button, Collapse } from 'react-bootstrap'
+import ReactMarkdown from 'react-markdown'
 const Job = ({ job }) => {
+  const [open, setOpen] = useState(false)
   return (
     <Card>
       <Card.Body>
@@ -12,8 +14,39 @@ const Job = ({ job }) => {
                 {job.company}
               </span>
             </Card.Title>
+            <Card.Subtitle className="text-muted mb-2">
+              {new Date(job.create_at).toLocaleDateString()}
+            </Card.Subtitle>
+            <Badge variant="secondary" className="mr-2">
+              {job.type}
+            </Badge>
+            <Badge variant="secondary">{job.location}</Badge>
+            <div style={{ wordBreak: 'break-all' }}>
+              <ReactMarkdown source={job.how_to_apply} />
+            </div>
+          </div>
+          <div>
+            <img
+              className="d-none d-md-block"
+              height="50"
+              alt={job.company}
+              src={job.company_logo}
+            />
           </div>
         </div>
+        <Card.Text>
+          <Button
+            onClick={() => setOpen((prevOpen) => !prevOpen)}
+            variant="primary"
+          >
+            {open ? 'Hide Details' : 'View Details'}
+          </Button>
+        </Card.Text>
+        <Collapse in={open}>
+          <div className="mt-4">
+            <ReactMarkdown source={job.description} />
+          </div>
+        </Collapse>
       </Card.Body>
     </Card>
   )

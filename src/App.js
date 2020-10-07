e9081cfc9466e10a1295fa84de38d9a86d25c497
rabobahago <rabobahago@gmail.com>
2020-10-07 08:45:22 +0100

    has next page added

--- src/App.js
+++ src/App.js
@@ -11,7 +11,7 @@ function App() {
   return (
     <Container className="my-4">
       <h1 className="mb-4">Github Jobs</h1>
-      <JobPagination page={page} setPage={setPage} />
+      <JobPagination page={page} setPage={setPage} hasNextPage={true} />
       {loading && <h1>loading....</h1>}
       {error && <h1> error, Try refreshing</h1>}
       <h1>
@@ -19,7 +19,7 @@ function App() {
           return <Job key={job.id} job={job} />
         })}
       </h1>
-      <JobPagination page={page} setPage={setPage} />
+      <JobPagination page={page} setPage={setPage} hasNextPage={true} />
     </Container>
   )
 }
--- src/JobPagination.js
+++ src/JobPagination.js
@@ -1,14 +1,16 @@
 import React from 'react'
 import { Pagination } from 'react-bootstrap'
 
-const JobPagination = ({ page, setPage }) => {
+const JobPagination = ({ page, setPage, hasNextPage }) => {
   return (
     <Pagination>
       {page !== 1 && <Pagination.Prev />}
-      {page !== 1 && <Pagination.Item>{page - 1}</Pagination.Item>}
-      <Pagination.Item>{page}</Pagination.Item>
-      <Pagination.Item>{page + 1}</Pagination.Item>
-      <Pagination.Next />
+      {page !== 1 && <Pagination.Item>1</Pagination.Item>}
+      {page > 2 && <Pagination.Ellipsis />}
+      {page > 2 && <Pagination.Item>{page - 1}</Pagination.Item>}
+      <Pagination.Item active>{page}</Pagination.Item>
+      {hasNextPage && <Pagination.Item>{page + 1}</Pagination.Item>}
+      {hasNextPage && <Pagination.Next />}
     </Pagination>
   )
 }
